4fd4ad6ee2724f368aac4e6129c22927ef89b4e0
Damjan Gogic <dadogogic@gmail.com>
2016-12-27 11:55:56 +0100

    Initial solution

--- DRS/CommonLibrary/Properties/AssemblyInfo.cs
+++ DRS/CommonLibrary/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ServiceModul1")]
+[assembly: AssemblyTitle("CommonLibrary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ServiceModul1")]
+[assembly: AssemblyProduct("CommonLibrary")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("78bf4fa7-392b-4613-974e-0128ed558340")]
+[assembly: Guid("93e0e8de-4566-4201-bdd4-1ad37dece1ee")]
 
 // Version information for an assembly consists of the following four values:
 //
