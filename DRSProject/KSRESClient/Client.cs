24aa0d2dbda8f563650d6cd518b7f15620867562
Luka Maric <luka94ns@gmail.com>
2016-12-29 00:48:12 +0100

    IssueCommand function initially implemented

--- DRSProject/KSRESClient/Client.cs
+++ DRSProject/KSRESClient/Client.cs
@@ -15,19 +15,52 @@ namespace KSRESClient
         private BindingList<Generator> generatorsForShowing;
         private List<LKResService> allUsers;
         private IKSForClient proxy = null;
+        private List<String> userNames;
+        private LKResService currentUser;
 
         public Client()
         {
             generatorsForShowing = new BindingList<Generator>();
             allUsers = new List<LKResService>();
-                DuplexChannelFactory<IKSForClient> factory = new DuplexChannelFactory<IKSForClient>(
-                        new InstanceContext(this),
-                            new NetTcpBinding(),
-                            new EndpointAddress("net.tcp://localhost:10020/IKSForClient"));
-                proxy = factory.CreateChannel();
+            UserNames = new List<string>();
+            userNames.Add("All");
+            currentUser = null;
+            for (int i = 0; i < 5; i++)
+            {
+                LKResService user = new LKResService("user" + (i + 1), null, null);
+                allUsers.Add(user);
+                userNames.Add(user.Username);
+            }
+            int j = 0;
+            for (int i = 1; i < 11; i++,j++)
+            {
+                Generator g = new Generator();
+                g.ActivePower = i * 100;
+                g.BasePoint = i * 100;
+                g.GeneratorType = GeneratorType.SOLAR;
+                g.HasMeasurment = true;
+                g.MRID = Guid.NewGuid().ToString();
+                g.Name = "gen" + i;
+                g.Pmax = i * 100;
+                g.Pmin = 0;
+                g.Price = i * 150;
+                g.SetPoint = i * 100;
+                g.WorkingMode = WorkingMode.REMOTE;
+                if(j == 5)
+                {
+                    j = 0;
+                }
+                allUsers[j].Generators.Add(g);
+            }
 
-                allUsers = proxy.GetAllSystem();
-                FillListForShowing();
+            /*DuplexChannelFactory<IKSForClient> factory = new DuplexChannelFactory<IKSForClient>(
+                    new InstanceContext(this),
+                        new NetTcpBinding(),
+                        new EndpointAddress("net.tcp://localhost:10020/IKSForClient"));
+            proxy = factory.CreateChannel();
+
+            allUsers = proxy.GetAllSystem();*/
+            FillListForShowing();
         }
         public BindingList<Generator> Generators
         {
@@ -41,8 +74,21 @@ namespace KSRESClient
                 RaisePropertyChanged("Generators");
             }
         }
-        
-        
+
+        public List<string> UserNames
+        {
+            get
+            {
+                return userNames;
+            }
+
+            set
+            {
+                userNames = value;
+                RaisePropertyChanged("Usernames");
+            }
+        }
+
         public void Update(UpdateInfo update, string username)
         {
             
@@ -119,13 +165,48 @@ namespace KSRESClient
         public void FillListForShowing()
         {
             generatorsForShowing.Clear();
-            foreach (LKResService user in allUsers)
+            if (currentUser == null)
             {
-                foreach (Generator g in user.Generators)
+                foreach (LKResService user in allUsers)
+                {
+                    foreach (Generator g in user.Generators)
+                    {
+                        generatorsForShowing.Add(g);
+                    }
+                }
+            }
+            else
+            {
+                foreach(Generator g in currentUser.Generators)
                 {
                     generatorsForShowing.Add(g);
                 }
             }
+        }
+
+        public void SetCurrentUser(string username)
+        {
+            if (username.Equals("All"))
+            {
+                currentUser = null;
+            }
+            else
+            {
+                foreach (LKResService user in allUsers)
+                {
+                    if (user.Username.Equals(username))
+                    {
+                        currentUser = user;
+                        break;
+                    }
+                }
+            }
+            FillListForShowing();
+        }
+
+        public void IssueCommand(string userName, double neededPower)
+        {
+            proxy.IssueCommand(userName, neededPower);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
--- DRSProject/KSRESClient/KSRESClientViewModel.cs
+++ DRSProject/KSRESClient/KSRESClientViewModel.cs
@@ -5,12 +5,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
 namespace KSRESClient
 {
-    public class KSRESClientViewModel
+    public class KSRESClientViewModel : INotifyPropertyChanged
     {
         private Client client;
         public Client Client
@@ -29,5 +30,84 @@ namespace KSRESClient
             }
         }
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+                if(propName.Equals("CbSelectedItem"))
+                {
+                    Client.SetCurrentUser(CbSelectedItem);
+                    IssueCommand.RaiseCanExecuteChanged();
+                }
+                else if(propName.Equals("NeededPower"))
+                {
+                    IssueCommand.RaiseCanExecuteChanged();
+                }
+            }
+        }
+
+        private String cbSelectedItem;
+
+        public String CbSelectedItem
+        {
+            get
+            {
+                return cbSelectedItem;
+            }
+            set
+            {
+                cbSelectedItem = value;
+                RaisePropertyChanged("CbSelectedItem");
+            }
+        }
+
+        private string neededPower;
+        public string NeededPower
+        {
+            get
+            {
+                return neededPower;
+            }
+            set
+            {
+                neededPower = value;
+                RaisePropertyChanged("NeededPower");
+
+            }
+        }
+
+        private DelegateCommand issueCommand;
+        public DelegateCommand IssueCommand
+        {
+            get
+            {
+                if (issueCommand == null)
+                {
+                    issueCommand = new DelegateCommand(IssueCommandAction, CanExecuteIssueCommand);
+                }
+
+                return issueCommand;
+            }
+        }
+
+        private bool CanExecuteIssueCommand()
+        {
+            return (!string.IsNullOrEmpty(NeededPower) && NeededPower !="All") && (CbSelectedItem != null && CbSelectedItem != "All");
+        }
+
+        private void IssueCommandAction()
+        {
+            try
+            {
+                Client.IssueCommand(CbSelectedItem, double.Parse(NeededPower));
+            }
+            catch
+            {
+                MessageBox.Show("Needed power must be number.");
+            }
+        }
     }
 }
