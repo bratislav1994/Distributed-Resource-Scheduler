9d01b403a77d20c5324cff6fd6b79804096d9a8b
Marko Stojkovic <mrkstjkvc4@gmail.com>
2016-12-28 08:25:59 +0100

    Impl services for LKRes

---
+++ DRSProject/LKRes/Services/LKClientService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterfaceForDuplex;
+
+namespace LKRes.Services
+{
+    public class LKClientService : ILKClient
+    {
+        public void Update(UpdateInfo update)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
