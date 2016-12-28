1428db91b4b25df6fd3f72628c795ce5fa32818a
Marko Stojkovic <mrkstjkvc4@gmail.com>
2016-12-28 19:23:38 +0100

    Modified LKForClientService, star testing LKForClientService class.

--- DRSProject/LKResTest/ServicesTest/LKForClientServiceTest.cs
+++ DRSProject/LKResTest/ServicesTest/LKForClientServiceTest.cs
@@ -3,11 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
+using CommonLibrary;
+using LKRes.Services;
 
 namespace LKResTest.ServicesTest
 {
+    [TestFixture]
     public class LKForClientServiceTest
     {
+        private LKForClientService lkResTest;
 
+        [OneTimeSetUp]
+        public void SetupTest()
+        {
+            this.lkResTest = new LKForClientService();
+        }
+
+        [Test]
+        public void ConstructorTest()
+        {
+            Assert.DoesNotThrow(() => new LKForClientService());
+        }
+
+        [Test]
+        public void GetMySystemTest()
+        {
+            Assert.DoesNotThrow(() => { lkResTest.GetMySystem(); });
+        }
+
+        [Test]
+        public void LoginTest(string usernameTest, string passwordTest)
+        {
+
+        }
+
+        [Test]
+        public void RegistrationTest(string usernameTest, string passwordTest)
+        {
+
+        }
+
+        [Test]
+        public void UpdateTest(UpdateInfo updateTest)
+        {
+
+        }
+
+        [Test]
+        public void AddTest(UpdateInfo updateTest)
+        {
+
+        }
+
+        [Test]
+        public void RemoveTest(UpdateInfo updateTest)
+        {
+
+        }
+
+        [Test]
+        public void UpdateDataTest(UpdateInfo updateTest)
+        {
+
+        }
     }
 }
