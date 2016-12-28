d0a9e0da4d77e8a8a5fae2f45dec6dd4f01fef8c
Damjan Gogic <dadogogic@gmail.com>
2016-12-28 11:04:50 +0100

    Classes modified

--- DRSProject/LKRes/Program.cs
+++ DRSProject/LKRes/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using CommonLibrary.Interfaces;
+using LKRes.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,14 @@ namespace LKRes
     {
         static void Main(string[] args)
         {
+            ServiceHost LKResHost = new ServiceHost(typeof(LKResService));
+            LKResHost.Open();
+
+            ServiceHost LKForClientHost = new ServiceHost(typeof(LKForClientService));
+            LKForClientHost.Open();
+
+            Console.WriteLine("Services are started...");
+            Console.ReadKey();
         }
     }
 }
