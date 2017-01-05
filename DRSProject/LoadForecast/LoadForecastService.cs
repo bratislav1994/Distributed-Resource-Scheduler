066cabedb1bda1d17caa407f769c903e6c33f42f
Luka Maric <luka94ns@gmail.com>
2017-01-05 10:52:49 +0100

    Loadforecast service started

---
+++ DRSProject/LoadForecast/LoadForecastService.cs
@@ -0,0 +1,21 @@
+﻿using CommonLibrary.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadForecast
+{
+    public class LoadForecastService : ILoadForecast
+    {
+        public List<double> LoadForecast(List<double> consumptions)
+        {
+            List<double> retVal = new List<double>(180);
+
+
+
+            return retVal;
+        }
+    }
+}
---
+++ DRSProject/LoadForecast/Program.cs
@@ -0,0 +1,28 @@
+﻿using CommonLibrary.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadForecast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetTcpBinding binding = new NetTcpBinding();
+            string address = "net.tcp://localhost:10040/IKSForClient";
+            ServiceHost host = new ServiceHost(typeof(LoadForecastService));
+            host.AddServiceEndpoint(typeof(ILoadForecast), binding, address);
+            host.Open();
+
+            Console.WriteLine("Service started, press <enter> to close...");
+
+            Console.ReadKey();
+
+            host.Close();
+        }
+    }
+}
